e3b1ef421e7314fbd0a777bd04b19dce1a556620
Christian Medina <christianmedina121@hotmail.com>
2022-11-17 15:04:56 -0500

    Updated .gitignore. Fixed exposed back4app client key as per Mohamed's PR review. Client key regenerated

--- app/src/main/java/com/example/starca/StarcaApplication.kt
+++ app/src/main/java/com/example/starca/StarcaApplication.kt
@@ -14,9 +14,9 @@ class StarcaApplication : Application() {
 
         Parse.initialize(
             Parse.Configuration.Builder(this)
-                .applicationId(getString(R.string.back4app_app_id))
-                .clientKey(getString(R.string.back4app_client_key))
-                .server(getString(R.string.back4app_server_url))
+                .applicationId(BuildConfig.BACK4APP_APP_ID)
+                .clientKey(BuildConfig.BACK4APP_CLIENT_KEY)
+                .server(BuildConfig.BACK4APP_SERVER_URL)
                 .build());
     }
 }
