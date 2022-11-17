47f3fa3dc01c7d5ba66c3b3bbea3339513f9af80
Christian Medina <christianmedina121@hotmail.com>
2022-11-16 19:18:06 -0500

    Added sign-in and register fragments. Updated README shields

--- app/src/main/java/com/example/starca/LoginActivity.kt
+++ app/src/main/java/com/example/starca/LoginActivity.kt
@@ -4,14 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.starca.fragments.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        findViewById<Button>(R.id.login_button).setOnClickListener {
-            goToMainActivity()
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        fragmentManager.beginTransaction().replace(R.id.login_fragment_container, SignInFragment()).commit()
+
+        // Bottom navigation view listener for switching between fragments
+        findViewById<BottomNavigationView>(R.id.login_bottom_nav_bar).setOnItemSelectedListener {
+                item ->
+
+            var currentFragment: Fragment? = null
+            when (item.itemId) {
+                R.id.login_bottom_nav_sign_in -> {
+                    currentFragment = SignInFragment()
+                }
+                R.id.login_bottom_nav_register -> {
+                    currentFragment = RegisterFragment()
+                }
+            }
+            if (currentFragment != null) {
+                fragmentManager.beginTransaction().replace(R.id.login_fragment_container, currentFragment).commit()
+            }
+            // This true signifies we handled the user interaction
+            true
         }
     }
 
@@ -21,6 +46,7 @@ class LoginActivity : AppCompatActivity() {
 
     private fun loginUser(){
         // TODO: finish setting up login
+        // goToMainActivity()
     }
 
     private fun goToMainActivity(){
---
+++ app/src/main/java/com/example/starca/fragments/SignInFragment.kt
@@ -0,0 +1,28 @@
+package com.example.starca.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.starca.R
+
+class SignInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.login_button).setOnClickListener {
+            //TODO: login button logic
+        }
+    }
+}
