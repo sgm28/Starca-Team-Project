4636e3fad6cf5b68850c04b125d38e034c76bdbf
sgm28 <sgm28@njit.edu>
2022-11-20 22:06:50 -0500

    feat: Completed image Upload for fragment Guide: Resources uses: Christen and his repo, CodePath YouTube videos https://github.com/xSynergyx/instagram-clone/blob/master/app/src/main/java/com/example/parsetagram/Post.kt https://www.youtube.com/watch?v=NgKsIJuVEuQ

--- app/src/main/java/com/example/starca/MainActivity.kt
+++ app/src/main/java/com/example/starca/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.starca
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -62,6 +61,8 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+
     }
 
     private fun signOut(){
