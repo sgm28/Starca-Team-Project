37dd399281500252f68bd1b0f42eaa4378212243
DrewH-NJIT <103283949+DrewH-NJIT@users.noreply.github.com>
2022-11-29 17:12:55 -0500

    Request Listing Update. 3

---
+++ app/src/main/java/com/example/starca/ListingRequest.kt
@@ -0,0 +1,35 @@
+package com.example.starca
+
+import android.os.Parcelable
+import android.util.Log
+import kotlinx.parcelize.Parcelize
+import org.json.JSONArray
+import org.json.JSONObject
+
+@Parcelize
+data class ListingRequest(
+    val objectId: String,
+    val status: Int
+) : Parcelable {
+
+    companion object {
+        fun fromJsonArray(requestJSONArray: JSONArray) : MutableList<ListingRequest> {
+
+            val requests = mutableListOf<ListingRequest>()
+            Log.e("DetailFragment", "fromJsonArray: $requestJSONArray", )
+
+            for (i in 0 until requestJSONArray.length()) {
+                val str = requestJSONArray.get(i).toString()
+                val requestJSON = JSONObject(str)
+
+                requests.add(
+                    ListingRequest(
+                        requestJSON.getString("objectId"),
+                        requestJSON.getInt("status")
+                    )
+                )
+            }
+            return requests
+        }
+    }
+}
