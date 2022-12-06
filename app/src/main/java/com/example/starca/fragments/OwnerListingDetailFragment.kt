d538ce9b759b153a7d70247bff3a3073acacd4d9
Mohamed Rachid <mohamednrachid@gmail.com>
2022-12-05 23:34:14 -0500

    New Listings Crash On Click In Profile
    Fixed issue where new listings that had not been requested would crash when viewing all the requests from the profile page.

--- app/src/main/java/com/example/starca/fragments/OwnerListingDetailFragment.kt
+++ app/src/main/java/com/example/starca/fragments/OwnerListingDetailFragment.kt
@@ -90,7 +90,7 @@ class OwnerListingDetailFragment : Fragment() {
 
         // Add the query constraints: Equal to the selected listing and has requests
         query.whereEqualTo("objectId", listing?.objectId)
-        query.whereNotEqualTo("listingRequests", null)
+//        query.whereNotEqualTo("listingRequests", null)
 
         query.findInBackground(object: FindCallback<Listing> {
             override fun done(listings: MutableList<Listing>?, e: ParseException?) {
@@ -101,10 +101,15 @@ class OwnerListingDetailFragment : Fragment() {
                     if (listings != null) {
                         val queriedListing : Listing = listings[0]
 
-                        val temp : MutableList<ListingRequest>? = listing!!.getJSONArray("listingRequests")
-                            ?.let { ListingRequest.fromJsonArray(it, listing!!.objectId) }
-                        val temp2 : MutableList<ListingRequest> = mutableListOf()
+                        var temp : MutableList<ListingRequest>? = mutableListOf()
+                        if (listing?.getJSONArray("listingRequests") == null) {
+                            temp = null
+                        } else {
+                            temp = listing!!.getJSONArray("listingRequests")
+                                ?.let { ListingRequest.fromJsonArray(it, listing!!.objectId) }
+                        }
 
+                        val temp2 : MutableList<ListingRequest> = mutableListOf()
 
                         if (temp != null) {
                             for (req in temp) {
